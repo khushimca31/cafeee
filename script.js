fedc09c3eb9f1496427327f1c6407dafacfbf3a9
Khushi <khushimca2025_31@ncrdsims.edu.in>
2026-05-05 16:39:09 +0530

    Added API Key

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
-let cart = [];
-let total = 0;
-
-function addToCart(item, price) {
-  cart.push({item, price});
-  total += price;
-  updateCart();
-}
-
-function updateCart() {
-  const cartList = document.getElementById("cart");
-  cartList.innerHTML = "";
-
-  cart.forEach(c => {
-    const li = document.createElement("li");
-    li.textContent = c.item + " - ₹" + c.price;
-    cartList.appendChild(li);
-  });
-
-  document.getElementById("total").textContent = total;
-}
-
-function placeOrder() {
-  alert("Order placed successfully! ");
-  cart = [];
-  total = 0;
-  updateCart();
-}
-
-function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+function placeOrder(e) {
+  e.preventDefault();
+  alert("Order placed successfully!");
 }
